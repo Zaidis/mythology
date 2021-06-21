c4da7825683b265887d6fd9f40c8c6d46aae9992
Zaidis <chewybinx@gmail.com>
2021-06-21 17:35:36 -0500

    good stuff

--- Mythology Game/Assets/Scripts/Player/PlayerManager.cs
+++ Mythology Game/Assets/Scripts/Player/PlayerManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
