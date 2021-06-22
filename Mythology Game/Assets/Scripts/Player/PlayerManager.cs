c1952b95db8c13eb5744cbe3223ae3ce4ded6674
Zaidis <chewybinx@gmail.com>
2021-06-21 20:29:16 -0500

    UI healthbar.
    added heal and damage functions to player manager

--- Mythology Game/Assets/Scripts/Player/PlayerManager.cs
+++ Mythology Game/Assets/Scripts/Player/PlayerManager.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float speed;
+    public float speed; //movement speed
+
+
     private void Awake() {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            Damage(1);
+        } else if (Input.GetKeyDown(KeyCode.Y)) {
+            Heal(1);
+        }
+
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
         rb.velocity = new Vector2(x * speed, y * speed);
     }
 
+    public int health { get; set; }
+    public void Damage(int amount) {
+        health -= amount;
+    }
+    public void Heal(int amount) {
+        health += amount;
+    }
 }
