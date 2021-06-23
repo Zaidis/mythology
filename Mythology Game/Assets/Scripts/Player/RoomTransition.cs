d09577e608639ec8bbdd15a763b4a3f503e5f378
Zaidis <chewybinx@gmail.com>
2021-06-22 23:31:47 -0500

    rooms

---
+++ Mythology Game/Assets/Scripts/Player/RoomTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+
+    public Transform location; //the spawnpoint where the player will go
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            GameObject obj = collision.gameObject;
+
+            obj.transform.position = location.position; //move player
+
+        }
+    }
+
+}
