09d0dcb0d8e7171f99bf8b83f387d3c6d075412a
Zaidis <chewybinx@gmail.com>
2021-06-26 14:12:31 -0500

    push

---
+++ Mythology Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health { get; set; }
+    public int damage { get; set; }
+
+    private void Start() {
+        health = 4;
+        damage = 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("BOLT")) {
+            health -= GameManager.instance.damage;
+            Destroy(collision.gameObject);
+            if(health <= 0) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
