55646e692248d30160a5b9b0edb1aa6884789d38
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2021-06-26 15:27:08 -0500

    fixed A*

---
+++ Mythology Game/Assets/Scripts/ThingsInDungeon/AStar2D/EnemyAstar.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAstar : MonoBehaviour
+{
+	public Transform target;
+	[SerializeField]
+	float speed = 5;
+	Vector3[] path;
+	int targetIndex;
+	Rigidbody2D rb;
+	public bool encounter = false;
+	Vector3 lastPosition;
+	Vector3 velocity;
+	Animator anim;
+	[SerializeField]
+	GameObject sight;
+	float reactionTime = 3;
+	GameObject player;
+	private void Start()
+	{
+		player = GameObject.Find("Player");
+		lastPosition = transform.position;
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	private void Update()
+	{
+		PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+	}
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+	{
+		if (pathSuccessful)
+		{
+			path = newPath;
+			targetIndex = 0;
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
+
+	IEnumerator FollowPath()
+	{
+		Vector3 currentWaypoint = path[0];
+		while (true)
+		{
+			if (transform.position == currentWaypoint)
+			{
+				targetIndex++;
+				if (targetIndex >= path.Length)
+				{
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+			}
+
+			//rb.transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+			//print(rb.velocity.x);
+			if (encounter == false)
+			{
+				if (lastPosition == transform.position)
+				{
+					transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+				}
+				else
+				{
+
+
+					transform.position = Vector2.MoveTowards(transform.position,currentWaypoint, speed * Time.fixedDeltaTime);
+					Vector3 direction = transform.position - lastPosition;
+					Vector3 localVelocity = transform.InverseTransformDirection(direction);
+					/*
+					if (direction.x < 0)
+					{
+						anim.SetBool("LoR", true);
+					}
+					else
+					{
+						anim.SetBool("LoR", false);
+					}
+					*/
+					//print(direction);
+					float angle = Mathf.Atan2(localVelocity.y, localVelocity.x) * Mathf.Rad2Deg;
+					Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+					sight.transform.rotation = Quaternion.Slerp(sight.transform.rotation, rotation, reactionTime);
+					//sight.transform.LookAt(Vector3.up+ sight.transform.rotation * direction);
+					anim.SetFloat("hVelocity", localVelocity.x);
+					anim.SetFloat("vVelocity", localVelocity.y);
+					lastPosition = transform.position;
+				}
+				yield return null;
+
+			}
+			if (encounter == true)
+			{
+				if (lastPosition == transform.position)
+				{
+					transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.fixedDeltaTime);
+				}
+				else
+				{
+
+
+					transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.fixedDeltaTime);
+					Vector3 direction = transform.position - lastPosition;
+					Vector3 localVelocity = transform.InverseTransformDirection(direction);
+					/*
+					if (direction.x < 0)
+					{
+						anim.SetBool("LoR", true);
+					}
+					else
+					{
+						anim.SetBool("LoR", false);
+					}
+					*/
+					print(direction);
+					float angle = Mathf.Atan2(localVelocity.y, localVelocity.x) * Mathf.Rad2Deg;
+					Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+					sight.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, reactionTime);
+					anim.SetFloat("hVelocity", direction.x);
+					anim.SetFloat("vVelocity", direction.y);
+					lastPosition = transform.position;
+				}
+				yield return null;
+			}
+			
+		}
+	}
+	IEnumerator lookAt(Vector3 direction)
+	{
+		while(true)
+		{
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			Quaternion rotation = Quaternion.AngleAxis(angle, sight.transform.position);
+			sight.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
+	public void OnDrawGizmos()
+	{
+		if (path != null)
+		{
+			for (int i = targetIndex; i < path.Length; i++)
+			{
+				Gizmos.color = Color.black;
+				Gizmos.DrawCube(path[i], Vector3.one);
+				
+
+				if (i == targetIndex)
+				{
+					Gizmos.DrawLine(transform.position, path[i]);
+				}
+				else
+				{
+
+					Gizmos.DrawLine(path[i - 1], path[i]);
+				}
+			}
+		}
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			//encounter = true;
+		}
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			//encounter = false;
+		}
+	}
+	
+}
---
+++ Mythology Game/Assets/Scripts/ThingsInDungeon/AStar2D/PathFinding.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PathFinding : MonoBehaviour
+{
+	Grid grid;
+
+	void Awake()
+	{
+		grid = GetComponent<Grid>();
+	}
+
+
+	
+
+	public void FindPath(PathRequest request, Action<PathResult> callback)
+	{
+
+		Vector3[] waypoints = new Vector3[0];
+		bool pathSuccess = false;
+
+		PathNode startNode = grid.NodeFromWorldPoint(request.pathStart);
+		PathNode targetNode = grid.NodeFromWorldPoint(request.pathEnd);
+
+
+		if (startNode.walkable && targetNode.walkable)
+		{
+			Heap<PathNode> openSet = new Heap<PathNode>(grid.MaxSize);
+			HashSet<PathNode> closedSet = new HashSet<PathNode>();
+			openSet.Add(startNode);
+
+			while (openSet.Count > 0)
+			{
+				PathNode currentNode = openSet.RemoveFirst();
+				closedSet.Add(currentNode);
+
+				if (currentNode == targetNode)
+				{
+					pathSuccess = true;
+					break;
+				}
+
+				foreach (PathNode neighbour in grid.GetNeighbors(currentNode))
+				{
+					if (!neighbour.walkable || closedSet.Contains(neighbour))
+					{
+						continue;
+					}
+
+					int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+					if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+					{
+						neighbour.gCost = newMovementCostToNeighbour;
+						neighbour.hCost = GetDistance(neighbour, targetNode);
+						neighbour.parent = currentNode;
+
+						if (!openSet.Contains(neighbour))
+							openSet.Add(neighbour);
+						else
+							openSet.UpdateItem(neighbour);
+					}
+				}
+			}
+		}
+		if (pathSuccess)
+		{
+			waypoints = RetracePath(startNode, targetNode);
+			pathSuccess = waypoints.Length > 0;
+		}
+		callback(new PathResult(waypoints, pathSuccess, request.callback));
+
+	}
+
+	Vector3[] RetracePath(PathNode startNode, PathNode endNode)
+	{
+		List<PathNode> path = new List<PathNode>();
+		PathNode currentNode = endNode;
+
+		while (currentNode != startNode)
+		{
+			path.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+		path.Add(startNode);
+		Vector3[] waypoints = SimplifyPath(path);
+		Array.Reverse(waypoints);
+		return waypoints;
+
+	}
+
+	Vector3[] SimplifyPath(List<PathNode> path)
+	{
+		List<Vector3> waypoints = new List<Vector3>();
+		Vector2 directionOld = Vector2.zero;
+
+		for (int i = 1; i < path.Count; i++)
+		{
+			Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+			if (directionNew != directionOld)
+			{
+				waypoints.Add(path[i - 1].worldPosition);
+			}
+			directionOld = directionNew;
+		}
+		return waypoints.ToArray();
+	}
+
+	int GetDistance(PathNode nodeA, PathNode nodeB)
+	{
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+		if (dstX > dstY)
+			return 14 * dstY + 10 * (dstX - dstY);
+		return 14 * dstX + 10 * (dstY - dstX);
+	}
+
+
+}
